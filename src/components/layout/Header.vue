2dae6df9031d885f02abf3058e6151153aeceb67
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2020-08-06 18:54:42 +0300

    finished the crash course

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="header">
+    <h1>TODO LIST:</h1>
+    <div class="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+
+<style scoped>
+.header {
+  background: #333;
+  color: #ffffff;
+  text-align: center;
+  padding: 0.7rem;
+}
+
+.header a {
+  color: #ffffff;
+  padding-right: 0.3rem;
+  text-decoration: none;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>About:</h1>
+    <p>
+      this the TodoList app verison V1.0.0, I is part of crash course on youtube
+    </p>
+  </div>
+</template>
